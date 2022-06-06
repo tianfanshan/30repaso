e6a2f24042a8db43acbc22d6744476e9436dcc7a
tianfanshan <stf30111994@gmail.com>
2022-06-06 10:54:30 +0200

    sd

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -10,6 +10,59 @@ const TaskController = {
             res.send(error)
         }
     },
+    async getAll(req,res){
+        try {
+            const tasks = await Task.find()
+            res.send(tasks)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send(error)
+        }
+    },
+    async getById(req,res){
+        try {
+            const task = await Task.findById(req.params._id)
+            res.send(task)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send(error)
+        }
+    },
+    async markAsCompleted(req,res){
+        try {
+            const taskCompleted = await Task.findByIdAndUpdate(
+                req.params._id,
+                {completed:true},
+                {new:true}
+                )
+            res.send({message:"Tu vaina ha sido completada",taskCompleted})
+        } catch (error) {
+            console.error(error)
+            res.status(500).send(error)
+        }
+    },
+    async update(req,res){
+        try {
+            const task = await Task.findByIdAndUpdate(
+                req.params._id,
+                {title:req.body.title},
+                {new:true}
+                )
+            res.send({message:"Ha mutado tu vaina loca mutante",task})
+        } catch (error) {
+            console.error(error)
+            res.status(500).send(error)
+        }
+    },
+    async delete(req,res){
+        try {
+            const taskdelete = await Task.findByIdAndDelete(req.params._id)
+            res.send('Tu vaina ha explotado locamente',taskdelete)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send(error)
+        }
+    }
 }
 
 module.exports = TaskController
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,5 +3,10 @@ const router = express.Router()
 const TaskController = require('../controller/TaskController')
 
 router.post('/',TaskController.create);
+router.get('/',TaskController.getAll);
+router.get('/id/:_id',TaskController.getById);
+router.put('/markAsCompleted/:_id',TaskController.markAsCompleted);
+router.put('/:_id',TaskController.update);
+router.delete('/:_id',TaskController.delete);
 
 module.exports = router
